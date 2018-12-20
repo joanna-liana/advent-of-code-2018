58c3a55c85e1a87708dcc92cdcbffc5398752bbf
Joanna Liana <joanna_liana@outlook.com>
2018-12-20 19:59:57 +0100

    Improve day 5, solution 1 + add solution 2

--- 5/index.js
+++ 5/index.js
@@ -31,6 +31,18 @@ const react = polymerString => {
   return newChars.join('');
 };
 
+const getLengthAfterFullReact = originalString => {
+  let inputString = originalString;
+  let outputString = react(inputString);
+
+  while (inputString !== outputString) {
+    inputString = outputString;
+    outputString = react(inputString);
+  }
+
+  return outputString.length;
+}
+
 let inputString = polymerString;
 let outputString = react(inputString);
 
@@ -39,5 +51,40 @@ while (inputString !== outputString) {
   outputString = react(inputString);
 }
 
-const result = outputString.length;
+const result = getLengthAfterFullReact(polymerString);
 console.log(result);
+
+
+// task 2
+// One of the unit types is causing problems;
+// it's preventing the polymer from collapsing as much as it should.
+// Your goal is to figure out which unit type is causing the most problems,
+// remove all instances of it (regardless of polarity),
+// fully react the remaining polymer, and measure its length.
+// e.g. remove a/A, b/B, etc.
+
+const alphabetLettersCount = 26;
+const lowerCaseStart = 'a'.charCodeAt();
+const upperCaseStart = 'A'.charCodeAt();
+
+Array.from({ length: 20 }, (x,i) => i + 97);
+
+let removalResults = {};
+
+for (let i = 0; i < alphabetLettersCount; i++) {
+  const letters = `${String.fromCharCode(i + lowerCaseStart)}|${String.fromCharCode(i + upperCaseStart)}`;
+  const replaceRegex = new RegExp(letters, 'g');
+  const parsedOutput = outputString.replace(replaceRegex, '');
+  removalResults[i] = getLengthAfterFullReact(parsedOutput);
+}
+
+let bestResult;
+
+// What is the length of the shortest polymer you can produce?
+Object.keys(removalResults).forEach(key => {
+  if (!bestResult || removalResults[key] < bestResult) {
+    bestResult = removalResults[key];
+  }
+});
+
+console.log(bestResult);
